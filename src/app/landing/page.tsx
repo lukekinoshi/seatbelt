ae4fdd9c194867b468a6d773a10f170bc81e15d8
luke kinoshi <lookmankinoshi85@gmail.com>
2026-06-26 08:26:50 -0400

    Fix landing page content

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -1,5 +1,4 @@
-import { redirect } from 'next/navigation'
+'use client'
+import { useRouter } from 'next/navigation'
 
-export default function Home() {
-  redirect('/landing')
-}
+function SeatBeltLogo...
