ad79ad37fda3e263e0a6f56c18cc4bc9eed8107d
luke kinoshi <lookmankinoshi85@gmail.com>
2026-06-26 07:29:23 -0400

    Fix redirect loop on homepage

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -1,5 +1,13 @@
-import { redirect } from 'next/navigation'
+'use client'
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
-  redirect('/landing')
+  const router = useRouter()
+  
+  useEffect(() => {
+    router.push('/landing')
+  }, [])
+
+  return null
 }
